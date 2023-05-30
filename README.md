0bb2e2f2d15ecc8d4621969da325ca80f64e41e2
sihoa2k1@gmail.com <sihoa2k1@gmail.com>
2023-05-30 09:29:23 +0700

    update ngay cf

---
+++ README.md
@@ -0,0 +1,1 @@
+## see more details at [Si Hoa's Page](https://shinsh0802.github.io/MyCV//)
---
+++ build/precache-manifest.95ec76c200eaa1adac32235c2e8db0d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561d93cb928e3a04ea6ffdfa4b1c5ee5",
+    "url": "/MyCV/index.html"
+  },
+  {
+    "revision": "e36ad01fc8783134cf68",
+    "url": "/MyCV/static/css/main.70b65b93.chunk.css"
+  },
+  {
+    "revision": "fc2f9f36b1681dc608a9",
+    "url": "/MyCV/static/js/2.1d797274.chunk.js"
+  },
+  {
+    "revision": "e36ad01fc8783134cf68",
+    "url": "/MyCV/static/js/main.86910fc2.chunk.js"
+  },
+  {
+    "revision": "c4c5bbd1c5932d3663ea",
+    "url": "/MyCV/static/js/runtime-main.532bd2f0.js"
+  },
+  {
+    "revision": "628fb69941eff1a950c7280c63e94a70",
+    "url": "/MyCV/static/media/avatar.628fb699.jpg"
+  }
+]);
